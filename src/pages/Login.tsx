c515fe8921d0b5776e8985054ae86ff791dd3a53
Yudota <yuri.fernandes.961@gmail.com>
2023-03-01 20:32:01 -0300

    login-v2

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -5,19 +5,18 @@ interface IndexProps { }
 const Login: React.FC<IndexProps> = (props) => {
   return (
     <>
-      <div className='container-fluid d-flex justify-content-around w-100 h-100' >
-        <div className='sm-4'>
+      <div className='container-fluid d-flex justify-content-around w-100 h-100 bg-gradient' >
+        <div>
           <img className='sm-6 auto' src='https://picsum.photos/id/480/900' alt="teste" />
         </div>
-        <form className='my-auto'>
-          <div className="form-group">
-            <label htmlFor="exampleInputEmail1">Email address</label>
-            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
-            <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+        <form className='my-auto '>
+          <div className="form-group gap-3">
+            <label htmlFor="exampleInputEmail1">Email</label>
+            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email" />
           </div>
           <div className="form-group">
-            <label htmlFor="exampleInputPassword1">Password</label>
-            <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
+            <label htmlFor="exampleInputPassword1">Senha</label>
+            <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Senha" />
           </div>
           <button type="submit" className="btn btn-primary">Submit</button>
         </form>
